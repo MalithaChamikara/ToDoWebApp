2f4b97530f024a4c1fd1dee4e864bcc99ae12cac
MalithaChamikara <malithachamikara@gmail.com>
2025-07-24 09:35:30 +0530

    Modified the ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Typography variant="h4" component="h1" gutterBottom align="center">
         To-Do App
       </Typography>
-      <Grid container spacing={4}>
+      <Grid container spacing={2}>
         <Grid item xs={12} md={3}>
           <TaskForm />
         </Grid>
